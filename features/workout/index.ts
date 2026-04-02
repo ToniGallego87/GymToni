ed981eda971502a90efbd951e3b0ae30ac16ef59
ToniGallego87 <tonigallego87@gmail.com>
2026-04-02 16:07:06 +0200

    Arreglo en App.tsx para que no pete al arrancar la apk

--- features/workout/index.ts
+++ features/workout/index.ts
@@ -1,4 +1,5 @@
 export { WorkoutProvider, WorkoutContext } from './WorkoutContext';
+export { useWorkout } from '@hooks/useWorkout';
 export { HomeScreen } from './HomeScreen';
 export { WorkoutLogScreen } from './WorkoutLogScreen';
 export { HistoryScreen } from './HistoryScreen';
